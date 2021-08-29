c14717c397da8e7013e27e682777c55486d57bea
Riccardo Feingold <riccardofeingold@yahoo.com>
2021-08-29 23:44:41 +0200

    added cool spline designs to all relevant pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => {
         <main>
             <Layout pageTitle="About Me">
                 <div className="container-fluid home-section justify-content-center">
-                    <StaticImage alt="Blog Title Page Image" src="../../images/pages/rocket.png" style={{maxWidth: `300px`, maxHeight: `300px`}} className="mx-auto d-block"/>
+                    <StaticImage alt="Blog Title Page Image" src="../images/pages/about.png" style={{maxWidth: `300px`, maxHeight: `300px`}} className="mx-auto d-block"/>
                     <h1 className="text-light text-center pt-3 pb-4">🧑‍💻 About Me</h1>
                     <h5 className="text-light fw-normal text-center pb-5">I'm currently a mechanical engineering student at the ETH of Zürich. As a side hustle I built this personal blog.</h5>
                 </div>
--- src/pages/my-impossible-list.js
+++ src/pages/my-impossible-list.js
@@ -6,7 +6,7 @@ const MyImpossibleList = () => {
     return (
         <Layout pageTitle="My Impossible List">
             <div className="container-fluid home-section justify-content-center">
-                <StaticImage alt="Blog Title Page Image" src="../../images/pages/blogging.png" style={{maxWidth: `300px`, maxHeight: `300px`}} className="mx-auto d-block"/>
+                <StaticImage alt="Blog Title Page Image" src="../images/pages/impossible-list.png" style={{maxWidth: `300px`, maxHeight: `300px`}} className="mx-auto d-block"/>
                 <h1 className="text-light text-center pt-3 pb-4">📜 My Impossible List</h1>
                 <h5 className="text-light fw-normal text-center pb-5">I like to blog about the stuff I'm interested in. Hopefully you'll find some of it interesting too.</h5>
             </div>
