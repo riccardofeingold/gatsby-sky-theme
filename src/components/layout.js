0e774daf5c4884e5bbaac609f151b52e5997ad2d
Riccardo Feingold <riccardofeingold@yahoo.com>
2021-08-31 00:12:31 +0200

    realized that I need search bar only for blog page, tuned navbar so that it is centered and without searchbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState } from "react";
+import React from "react";
 import Helmet from 'react-helmet'
 import { useStaticQuery, graphql} from 'gatsby'
 import './layout.scss'
@@ -32,24 +32,16 @@ function HomeTitleImage(props) {
 }
 
 const Layout = ({pageTitle, children}) => {
-    const searchField = useRef(null);
     const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    title
-                }
+    query {
+        site {
+            siteMetadata {
+                title
             }
-        }      
+        }
+    }      
     `)
 
-    const [isActive, setActive] = useState("false");
-
-    const ToggleClass = () => {
-        searchField.current.value = '';
-        searchField.current.focus();
-        setActive(!isActive);
-    }
     return (
         <main>
             {/* Header */}
@@ -58,49 +50,44 @@ const Layout = ({pageTitle, children}) => {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prism-themes@1.8.0/themes/prism-atom-dark.min.css"/>
             </Helmet>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <nav className="navbar navbar-expand-xl navbar-dark bg-primary p-3">
-                <a className="navbar-brand header-logo" href="/" style={HeaderLogo}>RICCARDORION</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars}/>
-                </button>
+            <div className="container-fluid bg-primary">
+                <div className="container">
+                    <nav className="navbar navbar-expand-xl navbar-dark p-3">
+                        <a className="navbar-brand header-logo" href="/" style={HeaderLogo}>RICCARDORION</a>
+                        
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <FontAwesomeIcon icon={faBars}/>
+                        </button>
 
-                <div className="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/about">About Me</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/portfolio">Portfolio</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/blog">Blog</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/#contact">Contact</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/my-impossible-list">My Impossible List</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/#contact">Hire Me</a>
-                        </li>
-                    </ul>
-
-                    <form className="form-inline d-flex position-search-bar" style={{width: `350px`}}>
-                        <div className={`search ${isActive ? null : `open`}`}>
-                            <input type="search" className="search-box" ref={(element) => {
-                                searchField.current = element;
-                            }}></input>
-                            <span role="searchbox" className="search-button" onClick={ToggleClass} onKeyDown={ToggleClass} tabIndex={0}>
-                                <FontAwesomeIcon icon={faSearch} className="search-icon"/>
-                            </span>
+                        <div className="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/about">About Me</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/portfolio">Portfolio</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/blog">Blog</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/#contact">Contact</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/my-impossible-list">My Impossible List</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/#contact">Hire Me</a>
+                                </li>
+                            </ul>
                         </div>
-                    </form>
+                    </nav>
                 </div>
-            </nav>
+            </div>
+            
 
             <HomeTitleImage isHome={pageTitle}/>
 
--- src/pages/blog/blog-list.js
+++ src/pages/blog/blog-list.js
@@ -1,21 +1,55 @@
 import { graphql, Link } from 'gatsby'
-import * as React from 'react'
+import React, {useRef, useState } from 'react'
 import Layout from '../../components/layout'
 import BlogCard from '../../components/blogcard'
 import { StaticImage } from 'gatsby-plugin-image'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
+
+  // const searchField = useRef(null);
+  // const [isActive, setActive] = useState("false");
+
+  // const ToggleClass = () => {
+  //     searchField.current.value = '';
+  //     searchField.current.focus();
+  //     setActive(!isActive);
+  // }
+
+  // const [state, setState] = useState({
+  //     filteredPosts: [],
+  //     query: "",
+  // });
+  
+  // const handleInputChange = event => {
+  //     const query = event.target.value;
+  //     const filteredPosts = allPosts.filter(post => {
+  //       const { description, title, tags } = post.node.frontmatter;
+  //       return (
+  //         description.toLowerCase().includes(query.toLowerCase()) ||
+  //         title.toLowerCase().includes(query.toLowerCase()) ||
+  //         (tags && tags.join("").toLowerCase().includes(query.toLowerCase()))
+  //       );
+  //     });
+  //     setState({
+  //       query,
+  //       filteredPosts,
+  //     });
+  // };
+  
+  // const posts = state.query ? state.filteredPosts : allPosts;
 
 class BlogPage extends React.Component {
   render() {
     const {data} = this.props
     const posts = data.allGhostPost.edges
-
+  
     // pagination 
     const { currentPage, numPages } = this.props.pageContext
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages || numPages == null
     const prevPage = currentPage - 1 === 1 ? "/blog" : (currentPage - 1).toString()
     const nextPage = (currentPage + 1).toString()
-
     return (
       <Layout pageTitle="My Blog Posts">
         <div className="container-fluid home-section justify-content-center">
@@ -23,6 +57,18 @@ class BlogPage extends React.Component {
           <h1 className="text-light text-center pt-3 pb-4">📝 Blog</h1>
           <h5 className="text-light fw-normal text-center pb-5">I like to blog about the stuff I'm interested in. Hopefully you'll find some of it interesting too.</h5>
         </div>
+
+        {/* <form className="form-inline d-flex position-search-bar" style={{width: `350px`}}>
+            <div className={`search ${isActive ? null : `open`}`}>
+                <input type="search" className="search-box" aria-label="Search" value={state.query} onChange={handleInputChange} ref={(element) => {
+                    searchField.current = element;
+                }}></input>
+                <span role="searchbox" className="search-button" onClick={ToggleClass} onKeyDown={ToggleClass} tabIndex={0}>
+                    <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+                </span>
+            </div>
+        </form> */}
+
         <div className="container py-3">
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
             {
