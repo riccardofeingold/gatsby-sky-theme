fb6e0953f2240663238e9484a308230fcc25465c
Riccardo Feingold <riccardofeingold@yahoo.com>
2021-09-16 15:40:45 +0200

    .gitignore should now work

--- versions/4.13.0/core/server/data/migrations/versions/4.12/01-add-email-only-column-to-posts-meta-table.js
+++
@@ -1,7 +0,0 @@
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('posts_meta', 'email_only', {
-    type: 'bool',
-    nullable: false,
-    defaultTo: false
-});
